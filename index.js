9f70bcd1e61a07e9804c9ac762af30414b4cf168
Paradise Bots <67729779+ParadiseBotList@users.noreply.github.com>
2020-09-30 18:09:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const fetch = require("node-fetch")
 class get {
     constructor(id, auth){
         if(!auth)throw new Error("Missing authorization token.")
-        if(!id)throw new Error("Missing bot id.")
+        if(!id)throw new Error("Missing Bot ID.")
         this.id = id
         this.auth = auth
     }
     async post(server_count, shard_count){
-        //At our api all actions logs is excuted from the bot list!
-        //You can't change any logs
-        //OH YEAH WHAT YOU ARE DOING HERE BITCH GET OUT NOW!
+        /* In our api all actions logs is excuted from the bot list!, You can't change any logs */
         let body = shard_count ? { 'server_count': server_count, 'shard_count': shard_count } : { 'server_count': server_count, 'shard_count': 0 }
         await fetch(`https://paradisebots.net/api/v1/bot/${this.id}`, {
             method: 'POST',
